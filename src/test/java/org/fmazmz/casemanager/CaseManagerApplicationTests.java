c9b285f9c3733adcd5b66cc6a1884d91aacc328d
WHITEROSE <firasmoussa60@gmail.com>
2026-03-24 17:44:17 +0100

    init

---
+++ src/test/java/org/fmazmz/casemanager/CaseManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fmazmz.casemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
