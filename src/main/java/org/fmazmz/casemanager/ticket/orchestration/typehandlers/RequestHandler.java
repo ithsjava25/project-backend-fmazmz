59965092a91e2078650045454a71a1dce0ff60f0
WHITEROSE <firasmoussa60@gmail.com>
2026-03-26 15:03:18 +0100

    implement tests for specific TypeHandlers

--- src/main/java/org/fmazmz/casemanager/ticket/orchestration/typehandlers/RequestHandler.java
+++ src/main/java/org/fmazmz/casemanager/ticket/orchestration/typehandlers/RequestHandler.java
@@ -3,7 +3,6 @@ package org.fmazmz.casemanager.ticket.orchestration.typehandlers;
 import org.fmazmz.casemanager.ticket.model.Priority;
 import org.fmazmz.casemanager.ticket.model.Ticket;
 import org.fmazmz.casemanager.ticket.model.TicketType;
-import org.fmazmz.casemanager.ticket.model.dto.CreateTicketRequest;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,7 +13,7 @@ public class RequestHandler implements TypeHandler {
     }
 
     @Override
-    public void applyDefaults(CreateTicketRequest request, Ticket ticket) {
+    public void applyDefaults(Ticket ticket) {
         if (ticket.getPriority() == null) ticket.setPriority(Priority.P5);
     }
 }
--- src/main/java/org/fmazmz/casemanager/ticket/orchestration/typehandlers/TypeHandler.java
+++ src/main/java/org/fmazmz/casemanager/ticket/orchestration/typehandlers/TypeHandler.java
@@ -2,10 +2,9 @@ package org.fmazmz.casemanager.ticket.orchestration.typehandlers;
 
 import org.fmazmz.casemanager.ticket.model.Ticket;
 import org.fmazmz.casemanager.ticket.model.TicketType;
-import org.fmazmz.casemanager.ticket.model.dto.CreateTicketRequest;
 
 public interface TypeHandler {
     TicketType supports();
 
-    void applyDefaults(CreateTicketRequest request, Ticket ticket);
+    void applyDefaults(Ticket ticket);
 }
