a7778c2974d8f592fa9ce70b63114a88fc89fb8b
Firas M. <149446898+fmazmz@users.noreply.github.com>
2026-03-26 15:06:58 +0100

    feat: Introduce Ticket types to seperate different cases for later scalability (#12)
    * implement TicketType and handlers
    
    * implement TypeHandlerFactory unit tests
    
    * implement tests for specific TypeHandlers

---
+++ src/main/java/org/fmazmz/casemanager/ticket/model/dto/CreateTicketRequest.java
@@ -0,0 +1,25 @@
+package org.fmazmz.casemanager.ticket.model.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import org.fmazmz.casemanager.ticket.model.TicketType;
+
+import java.util.UUID;
+
+public record CreateTicketRequest(
+        @NotBlank
+        @Size(min = 5, max = 100, message = "Title must be between 5 and 100 characters")
+        String title,
+
+        @NotBlank
+        @Size(max = 5000, message = "Description must be between 0 and 255 characters")
+        String description,
+
+        @NotNull
+        TicketType type,
+
+        @NotNull
+        UUID requesterId
+) {
+}
